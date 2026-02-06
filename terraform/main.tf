a9a6655be736c82818f361c675ee1933adceab38
nasreen-sudo <nasreennazz352@gmail.com>
2026-02-06 10:41:41 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -188,7 +188,7 @@ resource "aws_instance" "web" {
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
  
-  key_name = "dec-25-surya-mum"
+  key_name = "ec2"
  
   tags = {
     Name = "streamline-web-${count.index + 1}"
